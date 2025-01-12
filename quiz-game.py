848b8792ac4a8fffbfa7433eb3247a516ff0c5f3
Aanchal3134 <aanchal18shri@gmail.com>
2025-01-12 21:21:00 +0530

    initial commit

---
+++ quiz-game.py
@@ -0,0 +1,54 @@
+print("Welcome to the Fandom quiz !")
+
+playing = input("Do you want to play the game ? ") 
+
+if playing.lower() != "yes" :
+    quit()
+
+print("Okay ! Let's start the game :) ")    
+score = 0
+
+answer = input("Who is the Queen of Terrasen ? ")
+if answer.lower() == "aelin ashryver":
+    print("Correct ! ")
+    score += 1
+else: 
+    print("Incorrect !")
+
+
+answer = input("Who is the mother of dragons ? ")
+if answer.lower() == "daenerys targaryen":
+    print("Correct ! ")
+    score += 1
+else: 
+    print("Incorrect !")
+
+answer = input("Who won the Triwizard Tornament ? ")
+if answer.lower() == "harry potter":
+    print("Correct ! ")
+    score += 1
+else: 
+    print("Incorrect !")
+
+ 
+answer = input("Who is the Hero of ages ? ")
+if answer.lower() == "sazed":
+    print("Correct ! ")
+    score += 1
+else: 
+    print("Incorrect !")
+
+
+answer = input("Who is Kaladin's spren ? ")
+if answer.lower() == "syl":
+    print("Correct ! ")
+    score += 1
+else: 
+    print("Incorrect !")
+
+print("Your score is : " + str(score))
+print("Your percentage is : " + str((score/5) * 100) + "%.")
+
+
+
+
